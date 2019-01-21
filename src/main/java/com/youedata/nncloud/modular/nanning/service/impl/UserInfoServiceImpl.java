3d891946f1fcbce886412443a2578d86dd72ed58
malike <malike@youedata.com>
2019-01-21 18:45:04 +0800

    提交

--- src/main/java/com/youedata/nncloud/modular/nanning/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/youedata/nncloud/modular/nanning/service/impl/UserInfoServiceImpl.java
@@ -21,28 +21,30 @@ import java.util.Date;
  */
 @Service
 @Transactional(rollbackFor = java.lang.Exception.class)
-public class UserInfoServiceImpl extends ServiceImpl<BaseMapper<UserInfo>,UserInfo> implements IUserInfoService {
+public class UserInfoServiceImpl extends ServiceImpl<BaseMapper<UserInfo>, UserInfo> implements IUserInfoService {
     @Autowired
     UserInfoMapper userInfoMapper;
 
     /**
      * 用户登录
+     *
      * @param userInfoName
      * @param userinfoPwd
      */
     @Override
     public Object login(String userInfoName, String userinfoPwd) throws Exception {
         UserInfo userInfo = userInfoMapper.selectByTel(userInfoName);
-        if(userInfo == null || !userinfoPwd.equals(userInfo.getUserinfoPwd())){
+        if (userInfo == null || !userinfoPwd.equals(userInfo.getUserinfoPwd())) {
             throw new Exception("用户名或密码错误");
-        }else {
-            return null;
+        } else {
+            return userInfoMapper.selectMiniMessage(userInfoName);
         }
 
     }
 
     /**
      * 帮助注册
+     *
      * @param userInfoId
      * @param userinfoTel
      * @param userinfoWx
@@ -50,29 +52,35 @@ public class UserInfoServiceImpl extends ServiceImpl<BaseMapper<UserInfo>,UserIn
      * @param userinfoPwd
      */
     @Override
-    public void addUser(String userInfoId,String userinfoTel, String userinfoWx, String userinfoNickname, String userinfoPwd) {
-           //查询当前登录用户信息（推荐人信息）
-           UserInfo userInfo = userInfoMapper.selectById(userInfoId);
-           //新用户信息补全
-           UserInfo newUser = new UserInfo();
-           newUser.setUserinfoName(userinfoTel);//用户名就是手机号
-           newUser.setUserinfoTel(userinfoTel);
-           newUser.setUserinfoWx(userinfoWx);
-           newUser.setUserinfoNickname(userinfoNickname);
-           newUser.setUserinfoPwd(Encrypt.getMd5(userinfoPwd));
-           newUser.setUserinfoLv("1");
-           newUser.setUserinfoOrg(userInfo.getUserinfoOrg());
+    public void addUser(String userInfoId, String userinfoTel, String userinfoWx, String userinfoNickname, String userinfoPwd) throws Exception {
+        UserInfo user = userInfoMapper.selectByTel(userinfoTel);
+        if(user != null){//手机号查重
+            throw new Exception("该手机已注册");
+        }
 
-           String treeCode = userInfoMapper.getTreeCodeNext(userInfo.getUserinfoTreecode());
-           newUser.setUserinfoTreecode(userInfo.getUserinfoTreecode()+"-"+treeCode);
-           newUser.setUserinfoCreateBy(Integer.valueOf(userInfoId));
-           newUser.setUserinfoCreateTime(new Date());
-           newUser.insert();
+        //查询当前登录用户信息（推荐人信息）
+        UserInfo userInfo = userInfoMapper.selectById(userInfoId);
+        //新用户信息补全
+        UserInfo newUser = new UserInfo();
+        newUser.setUserinfoName(userinfoTel);//用户名就是手机号
+        newUser.setUserinfoTel(userinfoTel);
+        newUser.setUserinfoWx(userinfoWx);
+        newUser.setUserinfoNickname(userinfoNickname);
+        newUser.setUserinfoPwd(Encrypt.getMd5(userinfoPwd));
+        newUser.setUserinfoLv("1");
+        newUser.setUserinfoOrg(userInfo.getUserinfoOrg());
+
+        String treeCode = userInfoMapper.getTreeCodeNext(userInfo.getUserinfoTreecode());
+        newUser.setUserinfoTreecode(userInfo.getUserinfoTreecode() + "-" + treeCode);
+        newUser.setUserinfoCreateBy(Integer.valueOf(userInfoId));
+        newUser.setUserinfoCreateTime(new Date());
+        newUser.insert();
 
     }
 
     /**
      * 修改密码
+     *
      * @param userInfoId
      * @param oldPassord
      * @param newPassord
@@ -81,10 +89,10 @@ public class UserInfoServiceImpl extends ServiceImpl<BaseMapper<UserInfo>,UserIn
     public void changePwd(String userInfoId, String oldPassord, String newPassord) throws Exception {
         //查询当前登录用户信息
         UserInfo userInfo = userInfoMapper.selectById(userInfoId);
-        if(userInfo.getUserinfoPwd().equals(Encrypt.getMd5(oldPassord))){
+        if (userInfo.getUserinfoPwd().equals(Encrypt.getMd5(oldPassord))) {
             userInfo.setUserinfoPwd(Encrypt.getMd5(newPassord));
             userInfo.updateById();
-        }else{
+        } else {
             throw new Exception("原密码错误");
         }
 
